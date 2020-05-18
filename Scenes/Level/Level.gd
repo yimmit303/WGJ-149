5793e8e72b3003fe40c09d1ff62839a5816ebee8
yimmit303 <timsteiner303@gmail.com>
2020-05-17 23:04:26 -0400

    Fancy camera panning

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -10,29 +10,32 @@ var animated = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera.position = Vector2(960, -540)
-#	$Camera.zoom = Vector2(.25,.25)
+	$Camera.zoom = Vector2(.8, .8)
+	# TODO
+	$LevelTween.interpolate_property(	get_node("ParallaxBackground/BGParallax/SkyPlaceholder/DayCounter"), "modulate",
+										Color(1,1,1,0), Color(1,1,1,1), 3.0, Tween.TRANS_EXPO)
+	$LevelTween.interpolate_property(	get_node("ParallaxBackground/BGParallax/SkyPlaceholder/DayCounter"), "percent_visible",
+										0, 1, 3.0, Tween.TRANS_LINEAR)
+	$LevelTween.start()
+	yield($LevelTween, "tween_all_completed")
+	start_camera()
 	
 
 func _process(delta):
-	wait_a_sec -= delta
-	if wait_a_sec <= 0 and !animated:
-		animated = true
-		animate_camera()
-		yield($LevelTween, "tween_all_completed")
-		self.emit_signal("camera_finsished")
+	pass
 
 func start_camera():
-	animate_camera()
+	pan_camera()
 	yield($LevelTween, "tween_all_completed")
 	self.emit_signal("camera_finsished")
 
-func animate_camera():
+func pan_camera():
 	var animation_time = 2.0
+	var tween_type = Tween.TRANS_CUBIC
+	var ease_type = Tween.EASE_IN_OUT
 	$LevelTween.interpolate_property(	$Camera, "position", $Camera.position, Vector2(960, 540), 
-										animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
-#	$LevelTween.interpolate_property(	$Camera, "zoom", $Camera.zoom, Vector2(1,1), 
-#										animation_time, Tween.TRANS_BACK, Tween.EASE_OUT)
+										animation_time, tween_type, ease_type)
+	$LevelTween.interpolate_property(	$Camera, "zoom", $Camera.zoom, Vector2(1,1), 
+										animation_time, tween_type, ease_type)
 	$LevelTween.start()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
