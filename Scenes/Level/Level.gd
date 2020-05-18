6393c0205dc66c307509d9994623bf1f4f831f26
yimmit303 <timsteiner303@gmail.com>
2020-05-17 21:55:53 -0400

    Added panning camera

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 signal camera_finsished
 
-var cur_level = 1
+var days_left = 5
 
-var wait_a_sec = 1
+var wait_a_sec = 5
 var animated = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Camera.position = $Player.position
-	$Camera.zoom = Vector2(.25,.25)
+	$Camera.position = Vector2(960, -540)
+#	$Camera.zoom = Vector2(.25,.25)
+	
 
 func _process(delta):
 	wait_a_sec -= delta
@@ -20,12 +21,17 @@ func _process(delta):
 		yield($LevelTween, "tween_all_completed")
 		self.emit_signal("camera_finsished")
 
+func start_camera():
+	animate_camera()
+	yield($LevelTween, "tween_all_completed")
+	self.emit_signal("camera_finsished")
+
 func animate_camera():
-	var animation_time = 0.5
+	var animation_time = 2.0
 	$LevelTween.interpolate_property(	$Camera, "position", $Camera.position, Vector2(960, 540), 
-										animation_time, Tween.TRANS_BACK, Tween.EASE_OUT)
-	$LevelTween.interpolate_property(	$Camera, "zoom", $Camera.zoom, Vector2(1,1), 
-										animation_time, Tween.TRANS_BACK, Tween.EASE_OUT)
+										animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+#	$LevelTween.interpolate_property(	$Camera, "zoom", $Camera.zoom, Vector2(1,1), 
+#										animation_time, Tween.TRANS_BACK, Tween.EASE_OUT)
 	$LevelTween.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
