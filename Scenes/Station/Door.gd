56f69c460f5861c8fa631aad4b75620291da5268
yimmit303 <timsteiner303@gmail.com>
2020-05-18 19:00:58 -0400

    Made the door respond to NPCs

--- Scenes/Station/Door.gd
+++ Scenes/Station/Door.gd
@@ -1,12 +1,28 @@
 extends Node2D
 
+var open = false
+
 func _ready():
 	pass 
 
 func _on_OpenArea_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and open == false:
+		open = true
 		$DoorSprite.animation = "Open"
 
 func _on_OpenArea_body_exited(body):
-	if body.name == "Player":
+	if body.name == "Player" and open == true:
+		open = false
+		$DoorSprite.animation = "Close"
+
+
+func _on_OpenArea_area_entered(area):
+	if area.name == "NPCArea" and open == false:
+		open = true
+		$DoorSprite.animation = "Open"
+
+
+func _on_OpenArea_area_exited(area):
+	if area.name == "NPCArea" and open == true:
+		open = false
 		$DoorSprite.animation = "Close"
