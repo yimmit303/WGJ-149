06b1a0df35e278cb55baa4488462b5884b6fe785
yimmit303 <timsteiner303@gmail.com>
2020-05-19 19:05:15 -0400

    Made elevator move the player

--- Scenes/Station/Elevator.gd
+++ Scenes/Station/Elevator.gd
@@ -4,6 +4,7 @@ extends "res://Scenes/Interactables/Interactable.gd"
 # TODO: Make this move the player
 
 var open = false
+var moving = false
 var floor_num
 
 # Called when the node enters the scene tree for the first time.
@@ -11,12 +12,66 @@ func _ready():
 	pass # Replace with function body.
 
 func interact(event):
-	if event is InputEventKey and event.pressed == true and event.echo == false:
-		print("We are on floor: ", floor_num)
-		if event.scancode == KEY_W and floor_num < 5:
-			print("Going Up!")
-		if event.scancode == KEY_S and floor_num > 1:
-			print("Going Down!")
+	if !moving:
+		if event is InputEventKey and event.pressed == true and event.echo == false:
+			if event.scancode == KEY_W and floor_num < 5:
+				moving = true
+				
+				player.gravity = Vector2(0, 0)
+				player.velocity = Vector2(0, 0)
+				player.animator.animation = "Idle"
+				player.can_move = false
+				
+				$Tween.interpolate_property(player, "modulate", Color(1,1,1,1), Color(1,1,1,0), 0.5, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				
+				open = false
+				$ElevatorSprite.animation = "Close"
+	
+				
+				$Tween.interpolate_property(	player, "position", player.position, player.position + Vector2(0, -80),
+												1, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				
+				$Tween.interpolate_property(player, "modulate", Color(1,1,1,0), Color(1,1,1,1), 0.5, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				player.gravity = Vector2(0, 9.81)
+				player.can_move = true
+				
+				moving = false
+				
+			
+			if event.scancode == KEY_S and floor_num > 1:
+				moving = true
+				
+				player.gravity = Vector2(0, 0)
+				player.velocity = Vector2(0, 0)
+				player.animator.animation = "Idle"
+				player.can_move = false
+				
+				$Tween.interpolate_property(player, "modulate", Color(1,1,1,1), Color(1,1,1,0), 0.5, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				
+				open = false
+				$ElevatorSprite.animation = "Close"
+	
+				
+				$Tween.interpolate_property(	player, "position", player.position, player.position + Vector2(0, 80),
+												1, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				
+				$Tween.interpolate_property(player, "modulate", Color(1,1,1,0), Color(1,1,1,1), 0.5, Tween.TRANS_LINEAR)
+				$Tween.start()
+				yield($Tween, "tween_completed")
+				player.gravity = Vector2(0, 9.81)
+				player.can_move = true
+				
+				moving = false
 
 func _on_OpenArea_body_entered(body):
 	if body.name == "Player" and open == false:
