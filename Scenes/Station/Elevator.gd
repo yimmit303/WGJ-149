b9065c98e4323d55943d0e7067d81a750ed12acd
yimmit303 <timsteiner303@gmail.com>
2020-05-19 17:40:41 -0400

    Added interact funtion to elevator

--- Scenes/Station/Elevator.gd
+++ Scenes/Station/Elevator.gd
@@ -4,11 +4,20 @@ extends "res://Scenes/Interactables/Interactable.gd"
 # TODO: Make this move the player
 
 var open = false
+var floor_num
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
+
+func interact(event):
+	if event is InputEventKey and event.pressed == true and event.echo == false:
+		print("We are on floor: ", floor_num)
+		if event.scancode == KEY_W and floor_num < 5:
+			print("Going Up!")
+		if event.scancode == KEY_S and floor_num > 1:
+			print("Going Down!")
+
 func _on_OpenArea_body_entered(body):
 	if body.name == "Player" and open == false:
 		open = true
