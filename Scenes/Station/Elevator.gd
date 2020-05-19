5a8da2a95be9a4876bc798c022cacb1e8004bc11
yimmit303 <timsteiner303@gmail.com>
2020-05-19 15:04:44 -0400

    Update Elevator.gd

--- Scenes/Station/Elevator.gd
+++ Scenes/Station/Elevator.gd
@@ -3,21 +3,21 @@ extends Node2D
 
 # TODO: Make this move the player
 
+var open = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+	
 func _on_OpenArea_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and open == false:
+		open = true
 		$ElevatorSprite.animation = "Open"
 
 func _on_OpenArea_body_exited(body):
-	if body.name == "Player":
+	if body.name == "Player" and open == true:
+		open = false
 		$ElevatorSprite.animation = "Close"
+
 
