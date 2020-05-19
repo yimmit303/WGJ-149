3e6d2925e534e6a10befd981191fa0a35e366be3
yimmit303 <timsteiner303@gmail.com>
2020-05-19 14:35:31 -0400

    Uses new player ref

--- Scenes/Entities/Wires.gd
+++ Scenes/Entities/Wires.gd
@@ -4,5 +4,5 @@ var player_ref;
 
 
 func _ready():
-	player_ref = get_node("../Player");
+	player_ref = get_tree().get_root().get_child(0).get_player()
 	connect("body_entered", player_ref, "on_wire_trip");
