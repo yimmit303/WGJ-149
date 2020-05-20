7befd0f005ff11b17b7d216e46beb7a2b2bc5749
yimmit303 <timsteiner303@gmail.com>
2020-05-20 19:42:50 -0400

    Added cabinet interactable

---
+++ Scenes/Interactables/Cabinet.gd
@@ -0,0 +1,14 @@
+extends "res://Scenes/Interactables/Interactable.gd"
+
+export(int, 1, 5) var floor_num = 1
+
+func _ready():
+	type = "Cabinet"
+	details = [floor_num]
+	text_format = "Get files from the floor " + floor_num
+
+func interact(event):
+	if goal:
+		if event is InputEventKey and event.pressed == true and event.echo == false and event.scancode == KEY_E:
+			emit_signal("player_interacted", self, type, details)
+			player.inventory.append("Files")
